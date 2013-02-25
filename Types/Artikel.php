cc2ae3dcd407ed2462f33628992fefecbf582fb6
Tijs Verkoyen <tijs@verkoyen.eu>
2013-02-25 19:03:55 +0100

    PSR

--- Types/Artikel.php
+++ Types/Artikel.php
@@ -8,14 +8,225 @@ namespace TijsVerkoyen\DBFact\Types;
  */
 class Artikel extends BaseObject
 {
-    public $ArtCode, $ArtNummer, $VolgNummer, $Groep, $Soort, $Omschrijving,
-        $Omschrijving2, $Omschrijving3, $Omschrijving4, $Specificaties, $DetailSpecificaties,
-        $MerkId, $Merk, $StukNummer, $Opmerking, $Status, $StockControle, $VeldC1, $VeldC2,
-        $VeldC3, $VeldC4, $VeldC5, $VeldC6, $Website, $TopSeller, $VeldL3, $VeldL4, $VeldL5,
-        $BOKlant, $BOLeverancier, $Kleur, $Maat, $WebLink, $TypeArtikel, $Referentie, $Fabrikant,
-		$LotnummerArtikel, $ExtraTekstTaal1, $ExtraTekstTaal2, $ExtraTekstTaal3, $ExtraTekstTaal4,
-		$VeldM2, $VeldM3, $KortingsGroep;
+    /**
+     * @var string
+     */
+    public $ArtCode;
 
+    /**
+     * @var string
+     */
+    public $ArtNummer;
+
+    /**
+     * @var string
+     */
+    public $VolgNummer;
+
+    /**
+     * @var string
+     */
+    public $Groep;
+
+    /**
+     * @var string
+     */
+    public $Soort;
+
+    /**
+     * @var string
+     */
+    public $Omschrijving;
+
+    /**
+     * @var string
+     */
+    public $Omschrijving2;
+
+    /**
+     * @var string
+     */
+    public $Omschrijving3;
+
+    /**
+     * @var string
+     */
+    public $Omschrijving4;
+
+    /**
+     * @var string
+     */
+    public $Specificaties;
+
+    /**
+     * @var string
+     */
+    public $DetailSpecificaties;
+
+    /**
+     * @var string
+     */
+    public $MerkId;
+
+    /**
+     * @var string
+     */
+    public $Merk;
+
+    /**
+     * @var string
+     */
+    public $StukNummer;
+
+    /**
+     * @var string
+     */
+    public $Opmerking;
+
+    /**
+     * @var string
+     */
+    public $Status;
+
+    /**
+     * @var string
+     */
+    public $StockControle;
+
+    /**
+     * @var string
+     */
+    public $VeldC1;
+
+    /**
+     * @var string
+     */
+    public $VeldC2;
+
+    /**
+     * @var string
+     */
+    public $VeldC3;
+
+    /**
+     * @var string
+     */
+    public $VeldC4;
+
+    /**
+     * @var string
+     */
+    public $VeldC5;
+
+    /**
+     * @var string
+     */
+    public $VeldC6;
+
+    /**
+     * @var string
+     */
+    public $Website;
+
+    /**
+     * @var string
+     */
+    public $TopSeller;
+
+    /**
+     * @var string
+     */
+    public $VeldL3;
+
+    /**
+     * @var string
+     */
+    public $VeldL4;
+
+    /**
+     * @var string
+     */
+    public $VeldL5;
+
+    /**
+     * @var string
+     */
+    public $BOKlant;
+
+    /**
+     * @var string
+     */
+    public $BOLeverancier;
+
+    /**
+     * @var string
+     */
+    public $Kleur;
+
+    /**
+     * @var string
+     */
+    public $Maat;
+
+    /**
+     * @var string
+     */
+    public $WebLink;
+
+    /**
+     * @var string
+     */
+    public $TypeArtikel;
+
+    /**
+     * @var string
+     */
+    public $Referentie;
+
+    /**
+     * @var string
+     */
+    public $Fabrikant;
+
+    /**
+     * @var string
+     */
+    public $LotnummerArtikel;
+
+    /**
+     * @var string
+     */
+    public $ExtraTekstTaal1;
+
+    /**
+     * @var string
+     */
+    public $ExtraTekstTaal2;
+
+    /**
+     * @var string
+     */
+    public $ExtraTekstTaal3;
+
+    /**
+     * @var string
+     */
+    public $ExtraTekstTaal4;
+
+    /**
+     * @var string
+     */
+    public $VeldM2;
+
+    /**
+     * @var string
+     */
+    public $VeldM3;
+
+    /**
+     * @var string
+     */
+    public $KortingsGroep;
 
     /**
      * @var float
@@ -97,10 +308,10 @@ class Artikel extends BaseObject
      */
     public $NettoGewichtInKg;
 
-	/**
-	 * @var float
-	 */
-	public $VerpakkingsGewicht;
+    /**
+     * @var float
+     */
+    public $VerpakkingsGewicht;
 
     /**
      * @var int
