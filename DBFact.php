5aba7fb657411a05fae3677099ef593a8924a432
Tijs Verkoyen <tijs@verkoyen.eu>
2013-03-11 10:28:49 +0100

    Bumped version

--- DBFact.php
+++ DBFact.php
@@ -16,7 +16,7 @@ use TijsVerkoyen\DBFact\Types\Message;
 class DBFact extends BaseSoapClient
 {
     const DEBUG = false;
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     /**
      * The soapclient
