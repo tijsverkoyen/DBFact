a019ded83a246168113ecf97c4f79e8b1e6348dd
Jonas De Keukelaere <jonasdekeukelaere@gmail.com>
2013-06-27 13:37:21 +0200

    Implemented TransportFeeExtended and GetArtInfo

---
+++ Types/TransportFee.php
@@ -0,0 +1,30 @@
+<?php
+namespace TijsVerkoyen\DBFact\Types;
+
+/**
+ * DBFact Class TransportFee
+ *
+ * @author Jonas De Keukelaere <jonas@sumocoders.be>
+ */
+class TransportFee extends BaseObject
+{
+	/**
+	 * @var string
+	 */
+	public $TransportArticle;
+
+	/**
+	 * @var string
+	 */
+	public $TransportArticleCode;
+
+	/**
+	 * @var float
+	 */
+	public $TransportUpTo;
+
+	/**
+	 * @var float
+	 */
+	public $CustomTransportFee;
+}
